411415b52c73834c399274e100adcae5ab520817
praveen291 <mudadlapraveenkumar143@gmail.com>
2021-12-28 17:22:18 +0530

    pagenation page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+import Pagination from './Pagination';
 
-function App() {
+const App = () => {
+  const [data, setData] = useState([]);
+  const [perPage, setPerpage] = useState([]);
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/posts").then((res) => {
+      setData(res.data);
+      setPerpage(res.data.slice(0, 10));
+    });
+
+  }, [])
+  const pageHandler = (pageNumber) => {
+    setPerpage(data.slice(pageNumber*10)+10,pageNumber*10);
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div clssName="APP">
+      {data.length >= 1 ?
+        <div>
+          {perPage.map(post => <div className="post">{post.title}</div>)} <br />
+          <Pagination data={data}pageHandler={pageHandler} />
+
+        </div> :
+        <p>notloading</p>
+
+
+
+    }
+
+
     </div>
   );
 }
